e8cb518f9903c61ef4f029aa5d631005e481ef1f
Himanshu Gupta <hayhimanshu009@gmail.com>
2017-01-27 12:20:21 +0530

    handle server connection error

--- config/database.js
+++ config/database.js
@@ -6,17 +6,36 @@
 const mysql = require('mysql');
 //create connection object
 //heroku my-sql cleardb credential set
-const connection = mysql.createConnection({
-  //put your database credential
-  host     : '************',
-  user     : '*********',
-  password : '**********',
-  database : '************'
-});
-// Connect to the db
-connection.connect(function(err) {
-    if (err) throw err;
-});
+
+let db_config = {
+  host     : '***********',
+  user     : '**************',
+  password : '*********',
+  database : '**********'
+};
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 //Function exports
 module.exports = connection;
